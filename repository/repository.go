06caf6b262efdad5b139b24f4048dd6dff71de3f
dmitryk-dk <DAKozlov@luxoft.com>
2019-03-07 22:04:37 +0200

    change storage to repository

---
+++ repository/repository.go
@@ -0,0 +1,35 @@
+package repository
+
+import (
+	"database/sql"
+
+	"github.com/dmitryk-dk/chat/model"
+	"github.com/google/uuid"
+)
+
+// StorageInteface describe method for working with store
+type StorageInteface interface {
+	CreateUser(user model.User) error
+	GetUser(userID uuid.UUID) (model.User, error)
+
+	CreateMessage(msg model.Message) error
+	GetMessages(userID uuid.UUID) ([]model.Message, error)
+}
+
+// DB describe a store struct
+type DB struct {
+	db *sql.DB
+}
+
+// NewDB return constructor of database connection
+func NewDB(driverName, dataSourceName string) (*DB, error) {
+	db, err := sql.Open(driverName, dataSourceName)
+	defer db.Close()
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return &DB{db: db}, nil
+}
