9a9c6a48afd2850a0993d653486fd37cfd601a87
dmitryk-dk <DAKozlov@luxoft.com>
2019-04-14 13:36:35 +0300

    comment unused code

--- main.go
+++ main.go
@@ -4,11 +4,10 @@ import (
 	"flag"
 
 	"github.com/dmitryk-dk/chat/server"
-
-	_ "github.com/lib/pq"
+	// _ "github.com/lib/pq"
 )
 
-var addr = flag.String("addr", ":8080", "http service address") //flag.String("addr", ":8080", "http service address")
+var addr = flag.String("addr", ":8080", "http service address")
 
 func main() {
 	// db, err := repository.NewDB("postgres", "postgres://dmitryk:dmitryk@localhost:5432/chat?sslmode=disable")
