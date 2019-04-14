032ccb9e07d9c4e993bf1917d9a439e578284659
dmitryk-dk <DAKozlov@luxoft.com>
2019-04-14 13:34:15 +0300

    add server part for chart

--- main.go
+++ main.go
@@ -1,22 +1,27 @@
 package main
 
 import (
-	"log"
+	"flag"
 
-	repository "github.com/dmitryk-dk/chat/repository"
+	"github.com/dmitryk-dk/chat/server"
+
 	_ "github.com/lib/pq"
 )
 
+var addr = flag.String("addr", ":8080", "http service address") //flag.String("addr", ":8080", "http service address")
+
 func main() {
-	db, err := repository.NewDB("postgres", "postgres://dmitryk:dmitryk@localhost:5432/chat?sslmode=disable")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer repository.Close(db)
+	// db, err := repository.NewDB("postgres", "postgres://dmitryk:dmitryk@localhost:5432/chat?sslmode=disable")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer repository.Close(db)
 
-	err = repository.MigrateUp(db)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	// err = repository.MigrateUp(db)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	server.ServerHttp()
+	server.ServeWS()
+	server.ListenAndServ(addr)
 }
---
+++ server/client.go
@@ -0,0 +1,96 @@
+package server
+
+import (
+	"bytes"
+	"log"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+const (
+	writeWait      = 10 * time.Second
+	pongWait       = 60 * time.Second
+	pingPeriod     = (pongWait * 9) / 10
+	maxMessageSize = 512
+)
+
+var (
+	newLine = []byte{'\n'}
+	space   = []byte{' '}
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+type Client struct {
+	mid  *Middleware
+	conn *websocket.Conn
+	send chan []byte
+}
+
+func (c *Client) MessageRead() {
+	defer func() {
+		c.mid.unregister <- c
+		c.conn.Close()
+	}()
+	c.conn.SetReadLimit(maxMessageSize)
+	c.conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.conn.SetPongHandler(func(string) error { c.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+	c.MessageReader()
+}
+
+func (c *Client) MessageReader() {
+	for {
+		_, msg, err := c.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+		msg = bytes.TrimSpace(bytes.Replace(msg, newLine, space, -1))
+		c.mid.broadcast <- msg
+	}
+}
+
+func (c *Client) MessageWrite() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.conn.Close()
+	}()
+	for {
+		select {
+		case msg, ok := <-c.send:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+			w, err := c.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write(msg)
+			n := len(c.send)
+			for i := 0; i < n; i++ {
+				w.Write(newLine)
+				w.Write(<-c.send)
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			if err := c.conn.SetWriteDeadline(time.Now().Add(writeWait)); err != nil {
+				return
+			}
+			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
